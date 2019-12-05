cc83109c4bbecced097d08e85c991c0c86ffda3d
Alex Santillan <asantill68@yahoo.com>
2019-12-05 13:43:49 -0600

    OMBD and BandsInTown Working sans moment

--- liri.js
+++ liri.js
@@ -2,22 +2,32 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var axios = require("axios");
+//var moment = require("moment");
+//var Spotify = require("node-sotify-api");
+// var fs = require("fs");
+// var spotify = new Spotify({
+//     id:  keys.spotify.id,
+//     secret:  keys.spotify.secret,
+// });
 
 var command = process.argv[2];
 //console.log(command);
 
 var value = process.argv[3];
-console.log(value);
+//console.log(value);
 
 switch(command){
     case 'movie-this':
-        movie();
+        movie(value);
     break;
     case 'concert-this':
-        concert();
+        concert(value);
     break;
     case 'spotify-this-song':
-        song();
+        song(value);
+    break;
+    case 'do-what-it-says':
+        doWhatItSays()
     break;
     default:
         console.log("LIRI does not know that.");
@@ -28,15 +38,16 @@ function movie(){
     var queryUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + value;
     axios.get(queryUrl)
     .then(function(response){
+        var omdbR = response.data;
         console.log("---------------------------------------------------------------");
-        console.log("Movie Title: "+response.data.Title);
-        console.log("Year Movie Released: "+response.data.Year);
-        console.log("IMBD Rating: ", response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: ", response.data.Ratings[1].Value);
-        console.log("Country Produced: ", response.data.Country);
-        console.log("Language: ", response.data.Language);
-        console.log("Plot: ", response.data.Plot);
-        console.log("Actors: ", response.data.Actors);
+        console.log("           Movie Title: "+omdbR.Title);
+        console.log("   Year Movie Released: "+omdbR.Year);
+        console.log("           IMBD Rating: "+omdbR.imdbRating);
+        console.log("Rotten Tomatoes Rating: "+omdbR.Ratings[1].Value);
+        console.log("      Country Produced: "+omdbR.Country);
+        console.log("              Language: "+omdbR.Language);
+        console.log("                Actors: "+omdbR.Actors);
+        console.log("                  Plot: "+omdbR.Plot);
         console.log("---------------------------------------------------------------");
     })
 };
@@ -46,7 +57,18 @@ function concert(){
     var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
     axios.get(queryUrl)
     .then(function(response){
-        console.log(response.venue);
+        //var concertR = response.data[0];
+        console.log("---------------------------------------------------------------");
+        console.log("       Artist: "+value);
+        console.log("Name of Venue:  "+response.data[0].venue.name);
+        console.log(" Name of City:  "+response.data[0].venue.city +", " + response.data[0].venue.region);
+        // var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+         console.log("Date of Event: ");    //+eventDate);
+        console.log("---------------------------------------------------------------");
+        // for (var i = 0; i<response.data[i]; i++){
+        //     console.log(response.data[i].venue.name);
+        // }
+        
     });
 };
 
